cba1731ffe0d1e9c2976ceb708878f5c8a81f0ca
barrosGuihl <guilherme.barros@academico.ifpb.edu.br>
2024-12-01 08:31:58 -0300

    Media aritmética em java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,14 +4,16 @@ public class HelloWorld{
     public static void main(String[] args){
         Scanner numero = new Scanner(System.in);
 
-        System.out.println("adicione o valor 1: ");
-        int a = numero.nextInt();
-        System.out.println("adicione o valor 2: ");
-        int b = numero.nextInt();
+        System.out.println("adicione a nota 1: ");
+        int nota1 = numero.nextInt();
+        System.out.println("adicione a nota 2: ");
+        int nota2 = numero.nextInt();
+        System.out.println("adicione a nota 3: ");
+        int nota3 = numero.nextInt();
 
-        int soma = a + b;
+        int media = ((nota1 + nota2 + nota3)/3);
 
-        System.out.println("A soma e: " + soma);
+        System.out.println("A media e: " + media);
 
         numero.close();
     }
